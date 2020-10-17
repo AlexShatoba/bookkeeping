63ba9037f2eaf20ddd2506c67c0c8629857599a4
AlexShatoba <alexander.shatoba@saritasa.com>
2020-10-17 20:40:04 +0700

    Improve firebase firestore index.ts

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,20 @@
+import * as cors from 'cors';
+import * as express from 'express';
+import * as admin from 'firebase-admin';
+
 import * as functions from 'firebase-functions';
+
+admin.initializeApp();
+
+const app = express();
+
+app.use(cors({ origin: true }));
+
+app.post('/', async (req, res) => {
+  const user = req.body;
+  await admin.firestore().collection('users').add(user);
+  res.status(201).send();
+});
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiEndpoint: '',
+  apiEndpoint: 'https://bookkeeping-d0b62.firebaseio.com',
   assetsEndpoint: ''
 };
 
